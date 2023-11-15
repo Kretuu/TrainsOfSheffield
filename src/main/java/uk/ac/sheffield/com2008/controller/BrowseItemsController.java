bf1d8b3e6d34d91a6a5dbb21ff545d0098e5de6d
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-11-15 23:12:25 +0000

    comment System.out.println causing app to crash.

--- src/main/java/uk/ac/sheffield/com2008/controller/BrowseItemsController.java
+++ src/main/java/uk/ac/sheffield/com2008/controller/BrowseItemsController.java
@@ -9,7 +9,7 @@ public class BrowseItemsController extends ViewController {
     public BrowseItemsController(NavigationManager navigationManager, Navigation id){
         super(navigationManager, id);
         view = new BrowseItemsView(this);
-        System.out.println(AppSessionCache.getInstance().getUserLoggedIn().getBasket().toString());
+//        System.out.println(AppSessionCache.getInstance().getUserLoggedIn().getBasket().toString());
     }
 
 
