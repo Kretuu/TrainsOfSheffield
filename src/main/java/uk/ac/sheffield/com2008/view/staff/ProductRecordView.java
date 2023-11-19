95a1c6cfeca29003d758c43cc2c2054d271edf3d
aishasyafikaa <aishasyafikaa@gmail.com>
2023-11-19 00:44:43 +0000

    fix bug

--- src/main/java/uk/ac/sheffield/com2008/view/staff/ProductRecordView.java
+++ src/main/java/uk/ac/sheffield/com2008/view/staff/ProductRecordView.java
@@ -1,78 +1,91 @@
 package uk.ac.sheffield.com2008.view.staff;
 
 import uk.ac.sheffield.com2008.controller.staff.ProductRecordController;
+
 import uk.ac.sheffield.com2008.model.dao.ProductDAO;
 import uk.ac.sheffield.com2008.model.entities.Product;
+import uk.ac.sheffield.com2008.navigation.Navigation;
+import uk.ac.sheffield.com2008.view.View;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
+import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
-public class ProductRecordView extends StaffView {
+public class ProductRecordView extends View {
 
-    ProductRecordController controller;
-
+    ProductRecordController productRecordController;
     public ProductRecordView(ProductRecordController productRecordController) {
-        controller = productRecordController;
+        this.productRecordController = productRecordController;
         InitializeUI();
     }
 
     public void InitializeUI() {
+
         setLayout(new BorderLayout());
+        int padding = 40;
 
-        // Create a JPanel for the top section
-        JPanel topPanel = new JPanel(new BorderLayout());
-        topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        // top panel
+        JPanel topPanel = new JPanel(new GridLayout(2, 1));
 
-        //Create panel for the filter section
-        JPanel filterPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // Using FlowLayout to align components horizontally
-        filterPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
-
+        JPanel row1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
         //Create a label for products in stock
-        JLabel welcomeLabel = new JLabel("Product Records");
-        topPanel.add(welcomeLabel, BorderLayout.WEST);
+        JLabel viewLabel = new JLabel("Product Record");
+        row1.add(viewLabel);
+        topPanel.add(row1);
 
-        JButton staffAreaButton = new JButton("Logout");
-        topPanel.add(staffAreaButton, BorderLayout.EAST);
-
+        JPanel row2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel filterLabel = new JLabel("Filter by: ");
-        String[] filterOptions = {"Locomotive", "Carriage", "Wagon", "Controller", "Starter Oval TrackPack", "Extension TrackPack"};
+        String[] filterOptions = {"All Categories", "Locomotive", "Carriage", "Wagon", "Controller" };
         JComboBox<String> filterComboBox = new JComboBox<>(filterOptions);
+        JButton addRecordButton = new JButton("Create New Record");
+
         // Set tooltip for the combo box
         filterComboBox.setToolTipText("Select a category to filter the products");
-        filterPanel.add(filterLabel);
-        filterPanel.add(filterComboBox);
-        topPanel.add(filterPanel, BorderLayout.SOUTH);
-        add(topPanel, BorderLayout.NORTH);
+        row2.add(filterLabel);
+        row2.add(filterComboBox);
+        row2.add(addRecordButton);
+        row2.setBorder(BorderFactory.createEmptyBorder(0, padding, 0, 0));
+        topPanel.add(row2);
 
+        add(topPanel, BorderLayout.NORTH);
 
         // Create a JPanel for the scroll panel with product labels
         JPanel productPanel = new JPanel();
         productPanel.setLayout(new BoxLayout(productPanel, BoxLayout.Y_AXIS));
         productPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        String[] columnNames = {"Products in Stock", "Category", "Quantity", "Action"};
+        String[] columnNames = {"Product Code", "Product Name", "Category", "Price", "Quantity", "Action"};
 
         // Create a DefaultTableModel with column names and no data initially
         DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
 
         // Get products from the DAO
-        List<Product> products = ProductDAO.getAllProducts();
+        ArrayList<Product> products = (ArrayList<Product>) ProductDAO.getAllProducts();
 
         // Add each product to the tableModel
         for (Product product : products) {
-            Object[] rowData = {product.getName(), product.getStock(), "Edit"};
+            Object[] rowData = {product.getProductCode(), product.getName(), "Category", product.getPrice(),
+                    product.getStock(), "Edit"};
             tableModel.addRow(rowData);
         }
 
         // Create the JTable using the DefaultTableModel
         JTable table = new JTable(tableModel);
+        table.setEnabled(false);
 
-        // Optionally, you can add the JTable to a JScrollPane if needed
         JScrollPane scrollPane = new JScrollPane(table);
         productPanel.add(scrollPane);
         this.add(productPanel);
+        productPanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
+
+        // Create a JPanel for the bottom section with BoxLayout in Y_AXIS
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        JButton navigationButton = new JButton("Home");
+        bottomPanel.add(navigationButton);
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(0, 40, 0, 0));
+        navigationButton.addActionListener(e -> productRecordController.getNavigation().navigate(Navigation.STAFF));
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 
 }
