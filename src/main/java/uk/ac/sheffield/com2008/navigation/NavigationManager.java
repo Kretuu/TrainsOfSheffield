9b5c5b5651cb013e1c9205777f668dd76ce6da54
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2023-11-19 15:47:57 +0000

    Add onNavigateLeave function

--- src/main/java/uk/ac/sheffield/com2008/navigation/NavigationManager.java
+++ src/main/java/uk/ac/sheffield/com2008/navigation/NavigationManager.java
@@ -5,8 +5,10 @@ import uk.ac.sheffield.com2008.controller.auth.LoginController;
 import uk.ac.sheffield.com2008.controller.auth.SignupController;
 import uk.ac.sheffield.com2008.controller.customer.BasketController;
 import uk.ac.sheffield.com2008.controller.customer.BrowseItemsController;
+import uk.ac.sheffield.com2008.controller.customer.OrderHistoryController;
 import uk.ac.sheffield.com2008.controller.staff.ProductRecordController;
 import uk.ac.sheffield.com2008.controller.staff.StaffController;
+import uk.ac.sheffield.com2008.util.listeners.NavigationFrameWindowListener;
 import uk.ac.sheffield.com2008.view.components.MainLayout;
 import uk.ac.sheffield.com2008.view.View;
 import uk.ac.sheffield.com2008.view.auth.AuthView;
@@ -21,6 +23,7 @@ public class NavigationManager {
     private final JFrame frame;
     private final MainLayout layout;
     private final Map<Navigation, ViewController> controllers;
+    private Navigation currentView = Navigation.LOGIN;
 
     public NavigationManager() {
         frame = new JFrame("Trains of Sheffield");
@@ -28,12 +31,13 @@ public class NavigationManager {
         // Set the frame to fullscreen windowed mode
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.addWindowListener(new NavigationFrameWindowListener(this));
         frame.setVisible(true);
 
         controllers = new HashMap<>();
         registerControllers();
         //First screen after the app starts
-        navigate(Navigation.LOGIN);
+        navigate(currentView);
     }
 
     private void registerControllers() {
@@ -44,6 +48,7 @@ public class NavigationManager {
         new StaffController(this, Navigation.STAFF);
         new BasketController(this, Navigation.BASKET);
         new ProductRecordController(this,Navigation.PRODUCTRECORD);
+        new OrderHistoryController(this, Navigation.ORDER_HISTORY);
     }
 
     public void registerController(Navigation id, ViewController controller) {
@@ -51,6 +56,7 @@ public class NavigationManager {
     }
 
     public void navigate(Navigation id) {
+        controllers.get(currentView).onNavigateLeave();
         ViewController controller = controllers.get(id);
         View view = controller.getView();
         controller.onNavigateTo();
@@ -61,8 +67,13 @@ public class NavigationManager {
             if(!frame.getContentPane().equals(layout)) frame.setContentPane(layout);
             layout.setContent(view);
         }
+        currentView = id;
         frame.revalidate();
         frame.repaint();
 
+    }
+
+    public ViewController getCurrentController() {
+        return controllers.get(currentView);
     }
 }
