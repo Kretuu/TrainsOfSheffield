402ec0a6c4e8728e61f3407823e7f8bf4ffea232
aishasyafikaa <aishasyafikaa@gmail.com>
2023-11-26 17:06:05 +0000

    gui for train set

--- src/main/java/uk/ac/sheffield/com2008/controller/staff/FormController.java
+++ src/main/java/uk/ac/sheffield/com2008/controller/staff/FormController.java
@@ -1,18 +1,50 @@
 package uk.ac.sheffield.com2008.controller.staff;
 
 import uk.ac.sheffield.com2008.controller.ViewController;
+import uk.ac.sheffield.com2008.model.entities.Product;
 import uk.ac.sheffield.com2008.navigation.Navigation;
 import uk.ac.sheffield.com2008.navigation.NavigationManager;
 import uk.ac.sheffield.com2008.view.staff.ProductRecordForm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FormController extends ViewController {
+
+    private List<Product> allProducts;
 
     public FormController(NavigationManager navigationManager, Navigation id){
         super(navigationManager, id);
         view = new ProductRecordForm(this);
         ProductRecordForm productRecordForm = (ProductRecordForm) view;
 
+    }
+
+    public List<Product> getAllSelectedProducts(){
+       /* if(allProducts != null){
+            return allProducts;
+        }*/
+        return new ArrayList<>();
+    }
+
+    public String determineCustomCategory(String productCode) {
+        // Check if the productCode starts with the letter 'L'
+        if (productCode.startsWith("L")) {
+            return "Locomotive";
+        } else if (productCode.startsWith("C")) {
+            return "Controller";
+        } else if (productCode.startsWith("R")) {
+            return "Track";
+        } else if (productCode.startsWith("S")) {
+            return "Rolling Stocks";
+        } else if (productCode.startsWith("M")) {
+            return "Train Sets";
+        } else if (productCode.startsWith("P")) {
+            return "Train Packs";
+        } else {
+            return "Other Category";
+        }
     }
 
 }
--- src/main/java/uk/ac/sheffield/com2008/view/modals/ProductSetModal.java
+++ src/main/java/uk/ac/sheffield/com2008/view/modals/ProductSetModal.java
@@ -1,4 +1,28 @@
 package uk.ac.sheffield.com2008.view.modals;
 
-public class ProductSetModal {
+import uk.ac.sheffield.com2008.controller.staff.FormController;
+import uk.ac.sheffield.com2008.view.staff.ProductRecordForm;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class ProductSetModal extends JDialog {
+
+
+    public ProductSetModal(FormController formController, JFrame parentFrame){
+        super(parentFrame, "", true);
+
+        // Create a panel to hold the content
+        JPanel panel = new JPanel(new BorderLayout());
+
+        // Give the panel some padding
+        Border emptyBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        panel.setBorder(emptyBorder);
+
+        // Create a panel for the top section
+        JPanel itemsPanel = new JPanel(new BorderLayout());
+        JLabel selectLabel = new JLabel("Select Items: ");
+    }
+
 }
